0451537084ccd70156f908c022a93190e3efa0d6
Joe Orlando <61031447+archnewuser@users.noreply.github.com>
2020-11-06 16:37:17 -0500

    Add files via upload

---
+++ .config/sxhkd/show_help.sh
@@ -0,0 +1,2 @@
+#cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf
+cat ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu rofi -width 50 rofi -lines 33
